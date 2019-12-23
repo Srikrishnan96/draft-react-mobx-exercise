f5f12aa7ab12ab90460f669ccf1095f7785f163d
Srikrishna <krishna.srikrishna24@gmail.com>
2019-12-22 23:22:34 -0800

    Updates

---
+++ precache-manifest.7a56a53488da5febf73329b605a9b258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a807230c09875ad7336043e106a7679",
+    "url": "/draft-react-mobx-exercise/index.html"
+  },
+  {
+    "revision": "60130000e484f7d8eeb1",
+    "url": "/draft-react-mobx-exercise/static/css/main.8dec6cb0.chunk.css"
+  },
+  {
+    "revision": "4db4c5e09108800418af",
+    "url": "/draft-react-mobx-exercise/static/js/2.b166ff1b.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/draft-react-mobx-exercise/static/js/2.b166ff1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60130000e484f7d8eeb1",
+    "url": "/draft-react-mobx-exercise/static/js/main.029c2115.chunk.js"
+  },
+  {
+    "revision": "c3bbf77c06e594ec4652",
+    "url": "/draft-react-mobx-exercise/static/js/runtime-main.16bb264d.js"
+  }
+]);
